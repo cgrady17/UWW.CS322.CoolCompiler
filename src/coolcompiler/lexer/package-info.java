205943574d610882cf7750004d23b96baef67f0f
Connor Grady <cgrady17@outlook.com>
2016-02-15 17:11:57 -0600

    Updated package-info's

--- src/coolcompiler/lexer/package-info.java
+++ src/coolcompiler/lexer/package-info.java
@@ -2,5 +2,7 @@
  * Lexer module for the Cool Compiler
  *
  * @author Adam Brauns, Andrew Bronas, Brian Maresso, Connor Grady
+ * @since 1.0
+ * @version 1.0
  */
 package coolcompiler.lexer;
--- src/coolcompiler/parser/package-info.java
+++ src/coolcompiler/parser/package-info.java
@@ -2,5 +2,7 @@
  * Parser module for the Cool Compiler
  *
  * @author Adam Brauns, Andrew Bronas, Brian Maresso, Connor Grady
+ * @since 1.0
+ * @version 1.0
  */
 package coolcompiler.parser;
